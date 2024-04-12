04ba57d45aa40d1fff046fc6b43a13befe43165c
arphencallon <evgenius_your_emperor@mail.ru>
2024-04-12 18:43:20 +0300

    feat: add bundle analyze and single token page

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,17 +1,20 @@
+import BundleAnalyzer from '@next/bundle-analyzer'
+
+const withBundleAnalyzer = BundleAnalyzer({
+	enabled: process.env.ANALYZE === 'true',
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true, output: 'standalone',
-	webpack: (config, options) => {
+	reactStrictMode: true, output: 'standalone', webpack: (config, options) => {
 		config.module.rules.push({
-		  test: /\.txt/,
-		  type: 'asset/resource',
-		  generator: {
-			filename: 'static/[hash][ext]',
-		  },
+			test: /\.txt/, type: 'asset/resource', generator: {
+				filename: 'static/[hash][ext]',
+			},
 		})
-	
+
 		return config
 	},
 }
 
-export default nextConfig
+export default withBundleAnalyzer(nextConfig)
