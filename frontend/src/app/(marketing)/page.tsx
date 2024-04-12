80ad4fec65d6466f5e868861fbfbad651ad4ee2b
arphencallon <evgenius_your_emperor@mail.ru>
2024-04-12 17:48:42 +0300

    add basic animation to cta banner

--- frontend/src/app/(marketing)/page.tsx
+++ frontend/src/app/(marketing)/page.tsx
@@ -1,7 +1,7 @@
 import { Hero } from '@/components/sections/homepage/hero'
 import { HomepageFAQ } from '@/components/sections/homepage/faq'
 import { AllCoins } from '@/components/sections/homepage/all-coins'
-import { CreateTokenBanner } from '@/components/banners/cta-create-token'
+import { CreateTokenBanner } from '@/components/banners/create-token'
 import { About } from '@/components/sections/homepage/about'
 import { AboutCreate } from '@/components/sections/homepage/about-create'
 
@@ -11,9 +11,9 @@ export default function Page() {
 			<Hero />
 			<About />
 			<AboutCreate />
-			<div className="space-y-25 bg-[#C3C5EA] pb-25 pt-55">
+			<div className="space-y-25 overflow-hidden bg-[#C3C5EA] pb-25 pt-55">
 				<AllCoins />
-				<CreateTokenBanner className="" />
+				<CreateTokenBanner />
 			</div>
 			<HomepageFAQ />
 		</>
