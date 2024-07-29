f11ece037dd7a78973a0585e19c1d7659088c279
ted-ericcson <166023078+ted-ericcson@users.noreply.github.com>
2024-07-29 23:05:58 +0300

    Pinata Proxy (#22)
    * add pinata -proxy
    
    * Create build-pinata-to-k8s.yml (#24)
    
    * Update build-pinata-to-k8s.yml
    
    * Update build-pinata-to-k8s.yml
    
    * Update build-pinata-to-k8s.yml
    
    * Update Dockerfile
    
    ---------
    
    Co-authored-by: DDmitriy248 <166119701+DDmitriy248@users.noreply.github.com>

---
+++ pinata-proxy/src/pinata.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import PinataClient from '@pinata/sdk';
+import { RateLimiter } from 'limiter';
+
+@Injectable()
+export class PinataService {
+  private logger = new Logger(PinataService.name);
+  private readonly pinata: PinataClient;
+  private limiter: RateLimiter;
+
+  constructor(private configService: ConfigService) {
+    const apiKey = this.configService.getOrThrow<string>('PINATA_API_KEY');
+    const secretKey = this.configService.getOrThrow<string>(
+      'PINATA_SECRET_API_KEY',
+    );
+    this.limiter = new RateLimiter({
+      tokensPerInterval: 150,
+      interval: 'minute',
+    });
+    this.pinata = new PinataClient(apiKey, secretKey);
+  }
+
+  async uploadFile(file, fileName: string, retries = 0): Promise<string> {
+    try {
+      this.logger.log(`start to uploading file ${fileName} to IPFS`);
+      await this.limiter.removeTokens(1);
+      this.logger.log(`uploading file ${fileName} to IPFS`);
+      const pinataInfo = await this.pinata.pinFileToIPFS(file, {
+        pinataMetadata: {
+          name: fileName,
+        },
+        pinataOptions: { cidVersion: 0 },
+      });
+      this.logger.log(`file ${fileName} uploaded to IPFS`);
+      return pinataInfo.IpfsHash;
+    } catch (e) {
+      if (e.message.includes('429')) {
+        if (retries >= 10) {
+          throw e;
+        }
+        await new Promise((resolve) => setTimeout(resolve, 30 * 1000));
+        return this.uploadFile(file, fileName, retries + 1);
+      }
+    }
+  }
+}
